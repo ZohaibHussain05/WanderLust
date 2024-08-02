9848b4e53bf55603cbb5c8c20b1866f07e8f1f6a
Zohaib Hussain <chzohaibjutt300@gmail.com>
2024-08-02 17:26:46 +0500

    [DB] create listing model

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+
+main().then(() => {
+    console.log("connected to DB");
+}).catch((err) => {
+    console.log(err);
+})
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+};
+
+app.get("/testlistening", async(req,res) => {
+    let sampleListing = new Listing({
+        title: "My new House",
+        description: "Beautiful house between the town",
+        price: 120000,
+        location: "Kotla Arab Ali Khan",
+        country: "Pakistan",
+    });
+
+    await sampleListing.save();
+    console.log("sample was saved");
+    res.send("successfuly saved");
+})
 
 app.get("/", (req , res) => {
     res.send("Roat is working");
---
+++ models/listing.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schemma = mongoose.Schema;
+
+const listingSchema = new Schemma({
+    title: {
+        type:String,
+        required: true,
+    },
+    description: String,
+    image: {
+        type: String,
+        default: "https://images.unsplash.com/photo-1494526585095-c41746248156?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aG91c2V8ZW58MHx8MHx8fDA%3D",
+        set:(v) => v=== "" ? "https://images.unsplash.com/photo-1494526585095-c41746248156?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aG91c2V8ZW58MHx8MHx8fDA%3D":v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
