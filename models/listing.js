490ecd78a2656150bebad5790bf1503a77c316bf
Zohaib Hussain <chzohaibjutt300@gmail.com>
2024-08-11 19:58:41 +0500

    [Middleware] Delete Revies delete all review of listings when listing is deleted

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const review = require("./review");
+const Review = require("./review");
 const Schemma = mongoose.Schema;
 
 const listingSchema = new Schemma({
@@ -23,5 +23,11 @@ const listingSchema = new Schemma({
         },
     ]
 });
+
+listingSchema.post("findOneAndDelete",async(listing) => {
+    if(listing)
+        await Review.deleteMany({ _id: {$in: listing.reviews}});
+});
+
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
