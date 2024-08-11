e52c04db7703db233bc20b2924b2552959a0d43f
Zohaib Hussain <chzohaibjutt300@gmail.com>
2024-08-11 18:10:31 +0500

    [ErrorHandling] create conditon validator for reviews using Joi

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./schema.js");
+const {listingSchema , reviewSchema} = require("./schema.js");
 const Review = require("./models/review.js");
 
 main().then(() => {
@@ -29,6 +29,15 @@ app.use(express.static(path.join(__dirname,"/public")));
 
 const validateListing = (req,res ,next) => {
     let {error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }
+    next();
+};
+
+const validateReview = (req, res, next) => {
+    let {error} = reviewSchema.validate(req.body);
     if(error){
         let errMsg = error.details.map((el) => el.message).join(",");
         throw new ExpressError(400, errMsg);
@@ -85,7 +94,7 @@ app.delete("/listings/:id", wrapAsync(async(req, res) => {
 
 //Reviews
 //Post Route
-app.post("/listings/:id/reviews" ,wrapAsync( async(req, res) => {
+app.post("/listings/:id/reviews", validateReview ,wrapAsync( async(req, res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
 
